e62853f2fc33a32b2ca19f0b212eb7ba0871a184
Sean Breckenridge <seanbrecke@gmail.com>
2020-09-13 19:06:34 -0700

    sync meta-categories

---
+++ budget/budget/cleandata/transactions/meta_categories.py
@@ -0,0 +1,9 @@
+"""
+Specifies a map between the regular categories and larger ones
+"""
+
+META_CATEGORIES = {
+    "Food and Dining": "Food",
+    "Fast Food": "Food",
+    "Coffee Shops": "Food",
+}

878633716ceebef353fe068baf1200c199960669
Sean Breckenridge <seanbrecke@gmail.com>
2020-09-13 22:08:09 -0700

    negate total on checking, is opposite everything

--- scripts/fix_bank.py
+++ scripts/fix_bank.py
@@ -7,7 +7,7 @@ with open("/home/sean/Repos/mint/data/raw/bank_history.csv") as f:
         disc.append(line)
 acc_name = "Checking"
 disc = disc[1:]
-fixed = [[d[0], d[-2], d[1], acc_name, d[3]] for d in disc]
+fixed = [[d[0], -1 * float(d[-2]), d[1], acc_name, d[3]] for d in disc]
 with open("/home/sean/Repos/mint/data/old_transactions.csv", "a") as f:
     cs = csv.writer(f)
     for l in fixed:
